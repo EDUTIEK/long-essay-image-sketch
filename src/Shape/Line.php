1dc3443c7d7367cf5f7a080ba43f71eacb59d951
Lukas Scharmer <lscharmer@databay.de>
2026-01-30 14:15:39 +0100

    Change width of line

--- src/Shape/Line.php
+++ src/Shape/Line.php
@@ -11,7 +11,7 @@ class Line extends NoShape
 {
     private Point $end;
 
-    public const LINE_WIDTH = 10;
+    public const LINE_WIDTH = 17;
 
     public function __construct(Point $end, ...$args)
     {
